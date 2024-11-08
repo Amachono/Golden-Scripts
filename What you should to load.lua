9c95081374e6f4d5a9f854cca7858772537e9b3b
Amachono <faridbashirli2015f@gmail.com>
2024-11-08 20:26:20 +0400

    Create What you should to load.lua

---
+++ What you should to load.lua
@@ -0,0 +1,1 @@
+getgenv().ToggleKeybind = Enum.KeyCode.RightControl getgenv().FreeEmotesTab = true loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/Phantasm-Loader.lua"))()
